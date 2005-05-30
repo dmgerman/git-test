c1bb79b678986af9732bfae8dbfb784c5bf89552
Junio C Hamano <junkio@cox.net>
2005-05-30 00:09:07 -0700

    [PATCH] Add -O<orderfile> option to diff-* brothers.
    A new diffcore filter diffcore-order is introduced.  This takes
    a text file each of whose line is a shell glob pattern.  Patches
    that match a glob pattern on an earlier line in the file are
    output before patches that match a later line, and patches that
    do not match any glob pattern are output last.
    
    A typical orderfile for git project probably should look like
    this:
    
        README
        Makefile
        Documentation
        *.h
        *.c
    
    Signed-off-by: Junio C Hamano <junkio@cox.net>
    Signed-off-by: Linus Torvalds <torvalds@osdl.org>

---
+++ diffcore-order.c
@@ -0,0 +1,5 @@
+multiline_comment|/*&n; * Copyright (C) 2005 Junio C Hamano&n; */
+macro_line|#include &quot;cache.h&quot;
+macro_line|#include &quot;diff.h&quot;
+macro_line|#include &quot;diffcore.h&quot;
+macro_line|#include &lt;fnmatch.h&gt;
