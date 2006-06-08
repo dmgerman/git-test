7343990e06667be51857aa589f41d26cdc8996ed
Paul T Darga <pdarga@umich.edu>
2006-06-08 14:14:47 -0400

    check for error return from fork()
    Trivial fixup for fork() callsites which do not check for errors.
    
    Signed-off-by: Paul T Darga <pdarga@umich.edu>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- connect.c
+++ connect.c
@@ -3550,6 +3550,19 @@ suffix:semicolon
 r_if
 c_cond
 (paren
+id|pid
+OL
+l_int|0
+)paren
+id|die
+c_func
+(paren
+l_string|&quot;unable to fork&quot;
+)paren
+suffix:semicolon
+r_if
+c_cond
+(paren
 op_logical_neg
 id|pid
 )paren
