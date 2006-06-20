a6acbcaaf69509f11956cc4a1f0316727cbeb30c
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2006-06-20 09:51:09 +0200

    git_config: access() returns 0 on success, not > 0
    Another late-night bug. Sorry again.
    
    Signed-off-by: Johannes Schindelin <Johannes.Schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- config.c
+++ config.c
@@ -1610,6 +1610,7 @@ suffix:semicolon
 r_if
 c_cond
 (paren
+op_logical_neg
 id|access
 c_func
 (paren
@@ -1617,8 +1618,6 @@ id|user_config
 comma
 id|R_OK
 )paren
-OG
-l_int|0
 )paren
 id|ret
 op_assign
