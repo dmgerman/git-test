c52a5bff3b2eba18d8cef92feae6aecf09e6177f
Johannes Schindelin <Johannes.Schindelin@gmx.de>
2006-07-24 14:41:41 +0200

    Always reset the color _before_ printing out the newline
    This patch brings the benefits of part of v1.4.1-rc2~37
    to the "commit" colorizing patch.
    
    Signed-off-by: Johannes Schindelin <Johannes.Schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- log-tree.c
+++ log-tree.c
@@ -653,19 +653,6 @@ id|abbrev_commit
 )paren
 )paren
 suffix:semicolon
-id|putchar
-c_func
-(paren
-id|opt-&gt;commit_format
-op_eq
-id|CMIT_FMT_ONELINE
-ques
-c_cond
-l_char|&squot; &squot;
-suffix:colon
-l_char|&squot;&bslash;n&squot;
-)paren
-suffix:semicolon
 id|printf
 c_func
 (paren
@@ -678,6 +665,19 @@ id|opt-&gt;diffopt.color_diff
 comma
 id|DIFF_RESET
 )paren
+)paren
+suffix:semicolon
+id|putchar
+c_func
+(paren
+id|opt-&gt;commit_format
+op_eq
+id|CMIT_FMT_ONELINE
+ques
+c_cond
+l_char|&squot; &squot;
+suffix:colon
+l_char|&squot;&bslash;n&squot;
 )paren
 suffix:semicolon
 )brace
