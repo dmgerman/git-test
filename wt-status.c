b99d651f09af2e2f0f2fbaa42bc81f964f892273
Nicolas Pitre <nico@cam.org>
2007-01-13 21:23:55 -0500

    simplify the "no changes added to commit" message
    Suggesting the use of [-a|-i|-o] with git-commit is unnecessarily
    complex and confusing.  In this context -o is totally useless and -i
    requires extra arguments which are not mentioned.  The only sensible
    hint (besides reading the man page but let's not go there) is
    "commit -a".
    
    Signed-off-by: Nicolas Pitre <nico@cam.org>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- wt-status.c
+++ wt-status.c
@@ -1792,7 +1792,7 @@ id|s-&gt;workdir_dirty
 id|printf
 c_func
 (paren
-l_string|&quot;no changes added to commit (use &bslash;&quot;git add&bslash;&quot; and/or &bslash;&quot;git commit [-a|-i|-o]&bslash;&quot;)&bslash;n&quot;
+l_string|&quot;no changes added to commit (use &bslash;&quot;git add&bslash;&quot; and/or &bslash;&quot;git commit -a&bslash;&quot;)&bslash;n&quot;
 )paren
 suffix:semicolon
 r_else
