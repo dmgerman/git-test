b65268bf0e22222e301559f85a744281dba82caf
Tommi Kyntola <tommi.kyntola@ray.fi>
2007-02-16 10:50:58 +0200

    git-blame: prevent argument parsing segfault
    The 3rd branch in builtin-blame.c should also check for lacking
    arguments.  Running that in top dir does not trigger the problem
    because the 'prefix' is NULL.
    
    Signed-off-by: Tommi Kyntola <tommi.kyntola@ray.fi>
    Signed-off-by: Junio C Hamano <junkio@cox.net>

--- builtin-blame.c
+++ builtin-blame.c
@@ -10215,6 +10215,7 @@ c_cond
 id|seen_dashdash
 )paren
 (brace
+multiline_comment|/* (2) */
 r_if
 c_cond
 (paren
@@ -10275,6 +10276,19 @@ suffix:semicolon
 r_else
 (brace
 multiline_comment|/* (3) */
+r_if
+c_cond
+(paren
+id|argc
+op_le
+id|i
+)paren
+id|usage
+c_func
+(paren
+id|blame_usage
+)paren
+suffix:semicolon
 id|path
 op_assign
 id|add_prefix
