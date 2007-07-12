bde65b2c59e952e355e083fc5afdf6cf1552d5c5
Junio C Hamano <gitster@pobox.com>
2007-07-11 22:52:45 -0700

    Re-code builtin-branch.c in UTF-8
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-branch.c
+++ builtin-branch.c
@@ -1,4 +1,4 @@
-multiline_comment|/*&n; * Builtin &quot;git branch&quot;&n; *&n; * Copyright (c) 2006 Kristian H&#xfffd;gsberg &lt;krh@redhat.com&gt;&n; * Based on git-branch.sh by Junio C Hamano.&n; */
+multiline_comment|/*&n; * Builtin &quot;git branch&quot;&n; *&n; * Copyright (c) 2006 Kristian H&oslash;gsberg &lt;krh@redhat.com&gt;&n; * Based on git-branch.sh by Junio C Hamano.&n; */
 macro_line|#include &quot;cache.h&quot;
 macro_line|#include &quot;color.h&quot;
 macro_line|#include &quot;refs.h&quot;
