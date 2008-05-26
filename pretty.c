37e6701f4abf0f1606cb7a611a0afd97d919a2bf
Junio C Hamano <gitster@pobox.com>
2008-05-25 20:19:30 -0700

    log --pretty: do not accept bogus "--prettyshort"
    ... nor bogus "format.pretty = '=short'".  Both are syntax errors.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- pretty.c
+++ pretty.c
@@ -133,17 +133,6 @@ suffix:semicolon
 r_if
 c_cond
 (paren
-op_star
-id|arg
-op_eq
-l_char|&squot;=&squot;
-)paren
-id|arg
-op_increment
-suffix:semicolon
-r_if
-c_cond
-(paren
 op_logical_neg
 id|prefixcmp
 c_func
