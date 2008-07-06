09b9a51e7b03f3a63f5f15100860663a99a54bf5
Alex Riesen <raa.lkml@gmail.com>
2008-07-07 00:56:49 +0200

    git-clone: remove leftover debugging fprintf().
    Acked-by: Daniel Barkalow <barkalow@iabervon.org>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-clone.c
+++ builtin-clone.c
@@ -2286,16 +2286,6 @@ id|git_dir
 )paren
 )paren
 suffix:semicolon
-id|fprintf
-c_func
-(paren
-id|stderr
-comma
-l_string|&quot;Initialize %s&bslash;n&quot;
-comma
-id|git_dir
-)paren
-suffix:semicolon
 id|init_db
 c_func
 (paren
