d4c48bf81a652d7bc37ef631f29625de4dc8e36b
Junio C Hamano <gitster@pobox.com>
2008-08-19 18:05:43 -0700

    Build-in "git-shell"
    This trivially makes "git-shell" a built-in.  It makes the executable even
    fatter, though.
    
    And MinGW removed git-shell only because of the funny dependencies; there
    is no reason to do so anymore.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>
    Tested-on-MinGW-by: Johannes Sixt <johannes.sixt@telecom.at>

--- builtin-shell.c
+++ builtin-shell.c
@@ -2,6 +2,7 @@ macro_line|#include &quot;cache.h&quot;
 macro_line|#include &quot;quote.h&quot;
 macro_line|#include &quot;exec_cmd.h&quot;
 macro_line|#include &quot;strbuf.h&quot;
+macro_line|#include &quot;builtin.h&quot;
 DECL|function|do_generic_cmd
 r_static
 r_int
@@ -228,18 +229,24 @@ l_int|NULL
 comma
 )brace
 suffix:semicolon
-DECL|function|main
+DECL|function|cmd_shell
 r_int
-id|main
+id|cmd_shell
 c_func
 (paren
 r_int
 id|argc
 comma
+r_const
 r_char
 op_star
 op_star
 id|argv
+comma
+r_const
+r_char
+op_star
+id|prefix
 )paren
 (brace
 r_char
@@ -302,10 +309,14 @@ l_string|&quot;What do you think I am? A shell?&quot;
 suffix:semicolon
 id|prog
 op_assign
+id|xstrdup
+c_func
+(paren
 id|argv
 (braket
 l_int|2
 )braket
+)paren
 suffix:semicolon
 r_if
 c_cond
