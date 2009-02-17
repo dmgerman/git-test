3dd2f03a4122f1ba31986c0de9362a71220e69f1
Keith Cascio <keith@cs.ucla.edu>
2009-02-16 18:59:00 -0800

    Fix neglect of diff_setup()/diff_setup_done() symmetry.
    Code that calls diff_setup(), including via init_revisions(), should later call
    diff_setup_done(), possibly via setup_revisions(). Failure to do so could cause
    errors, especially in the future when we add responsibilities to
    diff_setup_done(). This instance causes no known errors with the present code.
    But it resulted in an error with an experimental patch.
    
    Signed-off-by: Keith Cascio <keith@cs.ucla.edu>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-checkout.c
+++ builtin-checkout.c
@@ -1692,6 +1692,24 @@ id|rev.diffopt.output_format
 op_or_assign
 id|DIFF_FORMAT_NAME_STATUS
 suffix:semicolon
+r_if
+c_cond
+(paren
+id|diff_setup_done
+c_func
+(paren
+op_amp
+id|rev.diffopt
+)paren
+OL
+l_int|0
+)paren
+id|die
+c_func
+(paren
+l_string|&quot;diff_setup_done failed&quot;
+)paren
+suffix:semicolon
 id|add_pending_object
 c_func
 (paren
