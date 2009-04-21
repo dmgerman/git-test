fff4feb03db1c92f683122e91a9a07cd402b46b5
Junio C Hamano <gitster@pobox.com>
2009-04-21 16:32:18 -0700

    Revert "stat_tracking_info(): only count real commits"
    This reverts commit 19de5d6913b9681d2bde533bccc8445c9236a648.
    It produces a misleading output to decide if a merge can fast-forward.

--- remote.c
+++ remote.c
@@ -7672,7 +7672,7 @@ id|ours
 r_return
 l_int|0
 suffix:semicolon
-multiline_comment|/* Run &quot;rev-list --no-merges --left-right ours...theirs&quot; internally... */
+multiline_comment|/* Run &quot;rev-list --left-right ours...theirs&quot; internally... */
 id|rev_argc
 op_assign
 l_int|0
@@ -7684,14 +7684,6 @@ op_increment
 )braket
 op_assign
 l_int|NULL
-suffix:semicolon
-id|rev_argv
-(braket
-id|rev_argc
-op_increment
-)braket
-op_assign
-l_string|&quot;--no-merges&quot;
 suffix:semicolon
 id|rev_argv
 (braket
