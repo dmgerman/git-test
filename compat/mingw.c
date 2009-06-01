df1a0a72dd54e454cfbe991afc8cff03ecd834e5
Johannes Schindelin <johannes.schindelin@gmx.de>
2009-06-01 08:41:45 +0200

    MinGW: fix warning about implicit declaration of _getch()
    conio.h provides the declaration.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Steffen Prohaska <prohaska@zib.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/mingw.c
+++ compat/mingw.c
@@ -1,5 +1,6 @@
 macro_line|#include &quot;../git-compat-util.h&quot;
 macro_line|#include &quot;win32.h&quot;
+macro_line|#include &lt;conio.h&gt;
 macro_line|#include &quot;../strbuf.h&quot;
 DECL|variable|_CRT_fmode
 r_int
