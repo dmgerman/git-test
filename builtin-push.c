de9615a40f27893ed5ba145b16ed1b4e1802ea27
Nanako Shiraishi <nanako3@lavabit.com>
2009-10-19 11:54:12 +0900

    git push: remove incomplete options list from help text
    'git push -h' shows usage text with incomplete list of options and then
    has a separate list of options that are supported. Imitate the way other
    commands (I looked at 'git diff' for an example) show their options.
    
    Signed-off-by: しらいし ななこ <nanako3@lavabit.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-push.c
+++ builtin-push.c
@@ -17,7 +17,7 @@ id|push_usage
 )braket
 op_assign
 (brace
-l_string|&quot;git push [--all | --mirror] [-n | --dry-run] [--porcelain] [--tags] [--receive-pack=&lt;git-receive-pack&gt;] [--repo=&lt;repository&gt;] [-f | --force] [-v] [&lt;repository&gt; &lt;refspec&gt;...]&quot;
+l_string|&quot;git push [&lt;options&gt;] [&lt;repository&gt; &lt;refspec&gt;...]&quot;
 comma
 l_int|NULL
 comma
