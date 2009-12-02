14e8185995ab96d5e1bc66e17c00a1ca67c8dd1d
Junio C Hamano <gitster@pobox.com>
2009-12-02 10:00:58 -0800

    merge: do not add standard message when message is given with -m option
    Even if the user explicitly gave her own message to "git merge", the
    command still added its standard merge message.  It resulted in a
    useless repetition like this:
    
        % git merge -m "Merge early part of side branch" <BACKTICK>git rev-parse side~2<BACKTICK>
        % git show -s
        commit 37217141e7519629353738d5e4e677a15096206f
        Merge: e68e646 a1d2374
        Author: しらいし ななこ <nanako3@lavabit.com>
        Date:   Wed Dec 2 14:33:20 2009 +0900
    
    	Merge early part of side branch
    
    	Merge commit 'a1d2374f8f52f4e8a53171601a920b538a6cec23'
    
    The gave her own message because she didn't want git to add the
    standard message (if she wanted to, she wouldn't have given one,
    or she would have prepared it using git-fmt-merge-msg command).
    
    Noticed by Nanako Shiraishi
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin-merge.c
+++ builtin-merge.c
@@ -261,7 +261,14 @@ c_func
 (paren
 id|buf
 comma
-l_string|&quot;%s&bslash;n&bslash;n&quot;
+l_string|&quot;%s%s&quot;
+comma
+id|buf-&gt;len
+ques
+c_cond
+l_string|&quot;&bslash;n&bslash;n&quot;
+suffix:colon
+l_string|&quot;&quot;
 comma
 id|arg
 )paren
@@ -5480,6 +5487,13 @@ op_assign
 l_string|&quot;HEAD&quot;
 suffix:semicolon
 multiline_comment|/*&n;&t;&t; * All the rest are the commits being merged;&n;&t;&t; * prepare the standard merge summary message to&n;&t;&t; * be appended to the given message.  If remote&n;&t;&t; * is invalid we will die later in the common&n;&t;&t; * codepath so we discard the error in this&n;&t;&t; * loop.&n;&t;&t; */
+r_if
+c_cond
+(paren
+op_logical_neg
+id|have_message
+)paren
+(brace
 r_for
 c_loop
 (paren
@@ -5534,6 +5548,7 @@ op_minus
 l_int|1
 )paren
 suffix:semicolon
+)brace
 )brace
 r_if
 c_cond
