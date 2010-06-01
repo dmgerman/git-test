3f79913a2328e21434df13584c308edbf9513b52
Jonathan Nieder <jrnieder@gmail.com>
2010-06-01 03:35:49 -0500

    DWIM 'git show -5' to 'git show --do-walk -5'
    To show the last two commits with one command, one might try
    
     1) git show -s master~2..
     2) git show -s ^master~2 master
     3) git show -s master^ master
     4) git show -s -2 master
    
    Choice (3) works because both commits are listed on the command line.
    Choices (1) and (2) have worked ever since v1.6.4-rc~3 (Make 'git
    show' more useful, 2009-07-13) disabled --no-walk in this case because
    there is no other useful meaning for them to have.  Unfortunately, (4)
    does not work: it outputs only one commit, because --no-walk stays on.
    
    So disable --no-walk in this case so ‘git show’ and future ‘git
    cherry-pick’ can behave as expected.
    
    As a side effect, this unfortunately changes the meaning of
    ‘git log --oneline --decorate --no-walk -5 --all’: instead of listing
    five refs, after this patch that command would list the five most
    recent commits.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- revision.c
+++ revision.c
@@ -5137,6 +5137,10 @@ op_plus
 l_int|12
 )paren
 suffix:semicolon
+id|revs-&gt;no_walk
+op_assign
+l_int|0
+suffix:semicolon
 )brace
 r_else
 r_if
@@ -5195,6 +5199,10 @@ op_plus
 l_int|1
 )paren
 suffix:semicolon
+id|revs-&gt;no_walk
+op_assign
+l_int|0
+suffix:semicolon
 )brace
 r_else
 r_if
@@ -5235,6 +5243,10 @@ l_int|1
 )braket
 )paren
 suffix:semicolon
+id|revs-&gt;no_walk
+op_assign
+l_int|0
+suffix:semicolon
 r_return
 l_int|2
 suffix:semicolon
@@ -5262,6 +5274,10 @@ id|arg
 op_plus
 l_int|2
 )paren
+suffix:semicolon
+id|revs-&gt;no_walk
+op_assign
+l_int|0
 suffix:semicolon
 )brace
 r_else
