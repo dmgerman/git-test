2073f853af26c4ffebc88c16c3c6ffb80a0ade2a
Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
2011-03-25 16:34:19 +0700

    Convert read_tree{,_recursive} to support struct pathspec
    This patch changes behavior of the two functions. Previously it does
    prefix matching only. Now it can also do wildcard matching.
    
    All callers are updated. Some gain wildcard matching (archive,
    checkout), others reset pathspec_item.has_wildcard to retain old
    behavior (ls-files, ls-tree as they are plumbing).
    
    Signed-off-by: Nguyễn Thái Ngọc Duy <pclouds@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/log.c
+++ builtin/log.c
@@ -1864,6 +1864,10 @@ r_struct
 id|setup_revision_opt
 id|opt
 suffix:semicolon
+r_struct
+id|pathspec
+id|match_all
+suffix:semicolon
 r_int
 id|i
 comma
@@ -1891,6 +1895,15 @@ l_int|1
 id|diff_use_color_default
 op_assign
 id|git_use_color_default
+suffix:semicolon
+id|init_pathspec
+c_func
+(paren
+op_amp
+id|match_all
+comma
+l_int|NULL
+)paren
 suffix:semicolon
 id|init_revisions
 c_func
@@ -2200,7 +2213,8 @@ l_int|0
 comma
 l_int|0
 comma
-l_int|NULL
+op_amp
+id|match_all
 comma
 id|show_tree_object
 comma
