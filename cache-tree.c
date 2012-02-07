52e197b998800640ee616a8c8f0730eb75666922
Junio C Hamano <gitster@pobox.com>
2012-02-07 11:55:48 -0800

    commit: ignore intent-to-add entries instead of refusing
    Originally, "git add -N" was introduced to help users from forgetting to
    add new files to the index before they ran "git commit -a".  As an attempt
    to help them further so that they do not forget to say "-a", "git commit"
    to commit the index as-is was taught to error out, reminding the user that
    they may have forgotten to add the final contents of the paths before
    running the command.
    
    This turned out to be a false "safety" that is useless.  If the user made
    changes to already tracked paths and paths added with "git add -N", and
    then ran "git add" to register the final contents of the paths added with
    "git add -N", "git commit" will happily create a commit out of the index,
    without including the local changes made to the already tracked paths. It
    was not a useful "safety" measure to prevent "forgetful" mistakes from
    happening.
    
    It turns out that this behaviour is not just a useless false "safety", but
    actively hurts use cases of "git add -N" that were discovered later and
    have become popular, namely, to tell Git to be aware of these paths added
    by "git add -N", so that commands like "git status" and "git diff" would
    include them in their output, even though the user is not interested in
    including them in the next commit they are going to make.
    
    Fix this ancient UI mistake, and instead make a commit from the index
    ignoring the paths added by "git add -N" without adding real contents.
    
    Based on the work by Nguyễn Thái Ngọc Duy, and helped by injection of
    sanity from Jonathan Nieder and others on the Git mailing list.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- cache-tree.c
+++ cache-tree.c
@@ -793,12 +793,6 @@ c_func
 (paren
 id|ce
 )paren
-op_logical_or
-(paren
-id|ce-&gt;ce_flags
-op_amp
-id|CE_INTENT_TO_ADD
-)paren
 )paren
 (brace
 r_if
@@ -1681,11 +1675,15 @@ c_cond
 (paren
 id|ce-&gt;ce_flags
 op_amp
+(paren
 id|CE_REMOVE
+op_or
+id|CE_INTENT_TO_ADD
+)paren
 )paren
 r_continue
 suffix:semicolon
-multiline_comment|/* entry being removed */
+multiline_comment|/* entry being removed or placeholder */
 id|strbuf_grow
 c_func
 (paren
