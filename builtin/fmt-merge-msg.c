0a8c41a1fa7d43114499da8b24719f7c3cc2b44f
Junio C Hamano <gitster@pobox.com>
2012-06-06 14:24:28 -0700

    fmt-merge-msg: make attribution into comment lines
    The submaintainer credit is not something you can compute purely by
    looking at the history and its shape, especially in the presense of
    fast-forward merges, and this observation makes the information on
    the "via" line unreliable.  Let's leave the final determination of
    credits up to whoever is making the merge and show them as comments.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/fmt-merge-msg.c
+++ builtin/fmt-merge-msg.c
@@ -1480,7 +1480,7 @@ l_char|&squot;a&squot;
 (brace
 id|label
 op_assign
-l_string|&quot;&bslash;nBy &quot;
+l_string|&quot;&bslash;n# By &quot;
 suffix:semicolon
 id|me
 op_assign
@@ -1495,7 +1495,7 @@ r_else
 (brace
 id|label
 op_assign
-l_string|&quot;&bslash;nvia &quot;
+l_string|&quot;&bslash;n# Via &quot;
 suffix:semicolon
 id|me
 op_assign
