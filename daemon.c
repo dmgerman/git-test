48706e655834886dd318033e25a1fb8288fb5a09
David Michael <fedora.dm0@gmail.com>
2013-02-25 14:30:19 -0500

    git-compat-util.h: Provide missing netdb.h definitions
    Some platforms may lack the NI_MAXHOST and NI_MAXSERV values in their
    system headers, so ensure they are available.
    
    Signed-off-by: David Michael <fedora.dm0@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- daemon.c
+++ daemon.c
@@ -8,10 +8,6 @@ macro_line|#ifndef HOST_NAME_MAX
 DECL|macro|HOST_NAME_MAX
 mdefine_line|#define HOST_NAME_MAX 256
 macro_line|#endif
-macro_line|#ifndef NI_MAXSERV
-DECL|macro|NI_MAXSERV
-mdefine_line|#define NI_MAXSERV 32
-macro_line|#endif
 macro_line|#ifdef NO_INITGROUPS
 DECL|macro|initgroups
 mdefine_line|#define initgroups(x, y) (0) /* nothing */
