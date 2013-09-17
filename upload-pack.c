e47d6774892817e5b88109a5d148570a7a979374
Junio C Hamano <gitster@pobox.com>
2013-09-17 16:21:33 -0700

    upload-pack: send non-HEAD symbolic refs
    With the same mechanism as used to tell where "HEAD" points at to
    the other end, we can tell the target of other symbolic refs as
    well.
    
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- upload-pack.c
+++ upload-pack.c
@@ -4135,6 +4135,15 @@ op_amp
 id|symref
 )paren
 suffix:semicolon
+id|for_each_namespaced_ref
+c_func
+(paren
+id|find_symref
+comma
+op_amp
+id|symref
+)paren
+suffix:semicolon
 r_if
 c_cond
 (paren
