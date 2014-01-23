acb1866313155003e4de513ce32ee5636201b3b6
Jeff King <peff@peff.net>
2014-01-23 16:23:09 -0500

    block-sha1: factor out get_be and put_be wrappers
    The BLK_SHA1 code has optimized wrappers for doing endian
    conversions on memory that may not be aligned. Let's pull
    them out so that we can use them elsewhere, especially the
    time-tested list of platforms that prefer each strategy.
    
    Signed-off-by: Jeff King <peff@peff.net>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- block-sha1/sha1.c
+++ block-sha1/sha1.c
@@ -29,18 +29,6 @@ macro_line|#else
 DECL|macro|setW
 mdefine_line|#define setW(x, val) (W(x) = (val))
 macro_line|#endif
-multiline_comment|/*&n; * Performance might be improved if the CPU architecture is OK with&n; * unaligned 32-bit loads and a fast ntohl() is available.&n; * Otherwise fall back to byte loads and shifts which is portable,&n; * and is faster on architectures with memory alignment issues.&n; */
-macro_line|#if defined(__i386__) || defined(__x86_64__) || &bslash;&n;    defined(_M_IX86) || defined(_M_X64) || &bslash;&n;    defined(__ppc__) || defined(__ppc64__) || &bslash;&n;    defined(__powerpc__) || defined(__powerpc64__) || &bslash;&n;    defined(__s390__) || defined(__s390x__)
-DECL|macro|get_be32
-mdefine_line|#define get_be32(p)&t;ntohl(*(unsigned int *)(p))
-DECL|macro|put_be32
-mdefine_line|#define put_be32(p, v)&t;do { *(unsigned int *)(p) = htonl(v); } while (0)
-macro_line|#else
-DECL|macro|get_be32
-mdefine_line|#define get_be32(p)&t;( &bslash;&n;&t;(*((unsigned char *)(p) + 0) &lt;&lt; 24) | &bslash;&n;&t;(*((unsigned char *)(p) + 1) &lt;&lt; 16) | &bslash;&n;&t;(*((unsigned char *)(p) + 2) &lt;&lt;  8) | &bslash;&n;&t;(*((unsigned char *)(p) + 3) &lt;&lt;  0) )
-DECL|macro|put_be32
-mdefine_line|#define put_be32(p, v)&t;do { &bslash;&n;&t;unsigned int __v = (v); &bslash;&n;&t;*((unsigned char *)(p) + 0) = __v &gt;&gt; 24; &bslash;&n;&t;*((unsigned char *)(p) + 1) = __v &gt;&gt; 16; &bslash;&n;&t;*((unsigned char *)(p) + 2) = __v &gt;&gt;  8; &bslash;&n;&t;*((unsigned char *)(p) + 3) = __v &gt;&gt;  0; } while (0)
-macro_line|#endif
 multiline_comment|/* This &quot;rolls&quot; over the 512-bit array */
 DECL|macro|W
 mdefine_line|#define W(x) (array[(x)&amp;15])
