7abbda05eeacfb8fe95654e48dbb05f9900b70f5
Elia Pinto <gitter.spiros@gmail.com>
2014-01-29 08:54:16 -0800

    builtin/fetch.c: reduce scope of variable
    Signed-off-by: Elia Pinto <gitter.spiros@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/fetch.c
+++ builtin/fetch.c
@@ -5416,9 +5416,6 @@ op_star
 id|argv
 )paren
 (brace
-r_int
-id|i
-suffix:semicolon
 r_static
 r_const
 r_char
@@ -5516,6 +5513,9 @@ r_int
 id|j
 op_assign
 l_int|0
+suffix:semicolon
+r_int
+id|i
 suffix:semicolon
 id|refs
 op_assign
