1124180a7b9ed227f6b4b67c8d128ea26b9b2271
Stefan Beller <stefanbeller@gmail.com>
2014-08-10 23:33:26 +0200

    prepare_revision_walk(): check for return value in all places
    Even the documentation tells us:
    
    	You should check if it returns any error (non-zero return
    	code) and if it does not, you can start using get_revision()
    	to do the iteration.
    
    In preparation for this commit, I grepped all occurrences of
    prepare_revision_walk and added error messages, when there were none.
    
    Signed-off-by: Stefan Beller <stefanbeller@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/commit.c
+++ builtin/commit.c
@@ -4789,11 +4789,24 @@ comma
 l_int|NULL
 )paren
 suffix:semicolon
+r_if
+c_cond
+(paren
 id|prepare_revision_walk
 c_func
 (paren
 op_amp
 id|revs
+)paren
+)paren
+id|die
+c_func
+(paren
+id|_
+c_func
+(paren
+l_string|&quot;revision walk setup failed&quot;
+)paren
 )paren
 suffix:semicolon
 id|commit
