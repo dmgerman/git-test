217488d2fb1f4fb4410526480918fcea639fd6f1
Ralf Thielow <ralf.thielow@gmail.com>
2015-01-20 20:30:28 +0100

    show-branch: fix indentation of usage string
    Noticed-by: Jean-Noël Avila <jn.avila@free.fr>
    Signed-off-by: Ralf Thielow <ralf.thielow@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/show-branch.c
+++ builtin/show-branch.c
@@ -18,9 +18,9 @@ id|N_
 c_func
 (paren
 l_string|&quot;git show-branch [-a|--all] [-r|--remotes] [--topo-order | --date-order]&bslash;n&quot;
-l_string|&quot;&t;&t;       [--current] [--color[=&lt;when&gt;] | --no-color] [--sparse]&bslash;n&quot;
-l_string|&quot;&t;&t;       [--more=&lt;n&gt; | --list | --independent | --merge-base]&bslash;n&quot;
-l_string|&quot;&t;       [--no-name | --sha1-name] [--topics] [(&lt;rev&gt; | &lt;glob&gt;)...]&quot;
+l_string|&quot;&t;&t;[--current] [--color[=&lt;when&gt;] | --no-color] [--sparse]&bslash;n&quot;
+l_string|&quot;&t;&t;[--more=&lt;n&gt; | --list | --independent | --merge-base]&bslash;n&quot;
+l_string|&quot;&t;&t;[--no-name | --sha1-name] [--topics] [(&lt;rev&gt; | &lt;glob&gt;)...]&quot;
 )paren
 comma
 id|N_
