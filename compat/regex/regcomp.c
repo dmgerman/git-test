98a36335d43959f537322e196efc498c57a7ecd0
Johannes Schindelin <johannes.schindelin@gmx.de>
2015-10-26 14:15:25 +0100

    Silence GCC's "cast of pointer to integer of a different size" warning
    When calculating hashes from pointers, it actually makes sense to cut
    off the most significant bits. In that case, said warning does not make
    a whole lot of sense.
    
    So let's just work around it by casting the pointer first to intptr_t
    and then casting up/down to the final integral type.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- compat/regex/regcomp.c
+++ compat/regex/regcomp.c
@@ -1,4 +1,5 @@
 multiline_comment|/* Extended regular expression matching and search library.&n;   Copyright (C) 2002-2007,2009,2010 Free Software Foundation, Inc.&n;   This file is part of the GNU C Library.&n;   Contributed by Isamu Hasegawa &lt;isamu@yamato.ibm.com&gt;.&n;&n;   The GNU C Library is free software; you can redistribute it and/or&n;   modify it under the terms of the GNU Lesser General Public&n;   License as published by the Free Software Foundation; either&n;   version 2.1 of the License, or (at your option) any later version.&n;&n;   The GNU C Library is distributed in the hope that it will be useful,&n;   but WITHOUT ANY WARRANTY; without even the implied warranty of&n;   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU&n;   Lesser General Public License for more details.&n;&n;   You should have received a copy of the GNU Lesser General Public&n;   License along with the GNU C Library; if not, write to the Free&n;   Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA&n;   02110-1301 USA.  */
+macro_line|#include &lt;stdint.h&gt;
 r_static
 id|reg_errcode_t
 id|re_compile_internal
@@ -11202,7 +11203,7 @@ r_void
 op_star
 )paren
 (paren
-r_int
+r_intptr
 )paren
 id|elem-&gt;token.opr.idx
 )paren
@@ -16442,7 +16443,7 @@ op_assign
 r_int
 )paren
 (paren
-r_int
+r_intptr
 )paren
 id|extra
 suffix:semicolon
--- sha1_file.c
+++ sha1_file.c
@@ -9488,6 +9488,9 @@ op_assign
 r_int
 r_int
 )paren
+(paren
+r_intptr
+)paren
 id|p
 op_plus
 (paren
